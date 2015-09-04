cba7ccb4bb5c149239661a826c1d0d2c206e6248
Allen Downey <downey@allendowney.com>
2015-09-04 15:09:17 -0400

    Adding nsfg.ipynb

--- nsfg.py
+++ nsfg.py
@@ -24,52 +24,52 @@ def ReadFemPreg(dct_file='2002FemPreg.dct',
     returns: DataFrame
     """
     dct = thinkstats2.ReadStataDct(dct_file)
-    df = dct.ReadFixedWidth(dat_file, compression='gzip')
-    CleanFemPreg(df)
-    return df
+    preg = dct.ReadFixedWidth(dat_file, compression='gzip')
+    CleanFemPreg(preg)
+    return preg
 
 
-def CleanFemPreg(df):
+def CleanFemPreg(preg):
     """Recodes variables from the pregnancy frame.
 
-    df: DataFrame
+    preg: DataFrame
     """
     # mother's age is encoded in centiyears; convert to years
-    df.agepreg /= 100.0
+    preg.agepreg /= 100.0
 
     # birthwgt_lb contains at least one bogus value (51 lbs)
     # replace with NaN
-    df.loc[df.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
+    preg.loc[preg.birthwgt_lb > 20, 'birthwgt_lb'] = np.nan
     
     # replace 'not ascertained', 'refused', 'don't know' with NaN
     na_vals = [97, 98, 99]
-    df.birthwgt_lb.replace(na_vals, np.nan, inplace=True)
-    df.birthwgt_oz.replace(na_vals, np.nan, inplace=True)
-    df.hpagelb.replace(na_vals, np.nan, inplace=True)
+    preg.birthwgt_lb.replace(na_vals, np.nan, inplace=True)
+    preg.birthwgt_oz.replace(na_vals, np.nan, inplace=True)
+    preg.hpagelb.replace(na_vals, np.nan, inplace=True)
 
-    df.babysex.replace([7, 9], np.nan, inplace=True)
-    df.nbrnaliv.replace([9], np.nan, inplace=True)
+    preg.babysex.replace([7, 9], np.nan, inplace=True)
+    preg.nbrnaliv.replace([9], np.nan, inplace=True)
 
     # birthweight is stored in two columns, lbs and oz.
     # convert to a single column in lb
     # NOTE: creating a new column requires dictionary syntax,
-    # not attribute assignment (like df.totalwgt_lb)
-    df['totalwgt_lb'] = df.birthwgt_lb + df.birthwgt_oz / 16.0    
+    # not attribute assignment (like preg.totalwgt_lb)
+    preg['totalwgt_lb'] = preg.birthwgt_lb + preg.birthwgt_oz / 16.0    
 
     # due to a bug in ReadStataDct, the last variable gets clipped;
     # so for now set it to NaN
-    df.cmintvw = np.nan
+    preg.cmintvw = np.nan
 
 
-def MakePregMap(df):
+def MakePregMap(preg):
     """Make a map from caseid to list of preg indices.
 
-    df: DataFrame
+    preg: DataFrame
 
     returns: dict that maps from caseid to list of indices into preg df
     """
     d = defaultdict(list)
-    for index, caseid in df.caseid.iteritems():
+    for index, caseid in preg.caseid.iteritems():
         d[caseid].append(index)
     return d
 
@@ -79,26 +79,26 @@ def main(script):
 
     script: string script name
     """
-    df = ReadFemPreg()
-    print(df.shape)
+    preg = ReadFemPreg()
+    print(preg.shape)
 
-    assert len(df) == 13593
+    assert len(preg) == 13593
 
-    assert df.caseid[13592] == 12571
-    assert df.pregordr.value_counts()[1] == 5033
-    assert df.nbrnaliv.value_counts()[1] == 8981
-    assert df.babysex.value_counts()[1] == 4641
-    assert df.birthwgt_lb.value_counts()[7] == 3049
-    assert df.birthwgt_oz.value_counts()[0] == 1037
-    assert df.prglngth.value_counts()[39] == 4744
-    assert df.outcome.value_counts()[1] == 9148
-    assert df.birthord.value_counts()[1] == 4413
-    assert df.agepreg.value_counts()[22.75] == 100
-    assert df.totalwgt_lb.value_counts()[7.5] == 302
+    assert preg.caseid[13592] == 12571
+    assert preg.pregordr.value_counts()[1] == 5033
+    assert preg.nbrnaliv.value_counts()[1] == 8981
+    assert preg.babysex.value_counts()[1] == 4641
+    assert preg.birthwgt_lb.value_counts()[7] == 3049
+    assert preg.birthwgt_oz.value_counts()[0] == 1037
+    assert preg.prglngth.value_counts()[39] == 4744
+    assert preg.outcome.value_counts()[1] == 9148
+    assert preg.birthord.value_counts()[1] == 4413
+    assert preg.agepreg.value_counts()[22.75] == 100
+    assert preg.totalwgt_lb.value_counts()[7.5] == 302
 
-    weights = df.finalwgt.value_counts()
+    weights = preg.finalwgt.value_counts()
     key = max(weights.keys())
-    assert df.finalwgt.value_counts()[key] == 6
+    assert preg.finalwgt.value_counts()[key] == 6
 
     print('%s: All tests passed.' % script)
 
